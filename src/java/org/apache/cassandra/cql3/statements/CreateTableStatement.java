b430c422b739b5d4745caba2fb9e03c311c0b9e0
Matthew <matthew@MatthewYang.(none)>
2014-01-20 17:08:39 +0800

    add notes about CreateTableStatement.java

--- src/java/org/apache/cassandra/cql3/statements/CreateTableStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/CreateTableStatement.java
@@ -43,14 +43,17 @@ import org.apache.cassandra.utils.ByteBufferUtil;
 /** A <code>CREATE TABLE</code> parsed from a CQL query statement. */
 public class CreateTableStatement extends SchemaAlteringStatement
 {
+	//clustering keys comparator or UTF8 or CompositeKey
     public AbstractType<?> comparator;
     private AbstractType<?> defaultValidator;
     private AbstractType<?> keyValidator;
-
+    //partition keys which are first columns in primary keys
     private final List<ByteBuffer> keyAliases = new ArrayList<ByteBuffer>();
+    //clustering keys, which are primary keys exception partition keys
     private final List<ByteBuffer> columnAliases = new ArrayList<ByteBuffer>();
+    //clustering key name(the clustering key is only one, and compact storage), or empty buffer(compact storage, only primary key), or null 
     private ByteBuffer valueAlias;
-
+    // contain all columns not including primary key
     private final Map<ColumnIdentifier, AbstractType> columns = new HashMap<ColumnIdentifier, AbstractType>();
     private final CFPropDefs properties;
     private final boolean ifNotExists;
